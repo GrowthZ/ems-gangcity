676a546cdc320a043170dac1c85be119f0a81dae
duthanhhoang <duthanhhoang@gmail.com>
2024-06-26 16:33:28 +0700

    update student

--- src/pages/attendances/AttendancePage.vue
+++ src/pages/attendances/AttendancePage.vue
@@ -104,7 +104,7 @@ data.load(DataSheet.calendar, [DataSheet.student, DataSheet.location, DataSheet.
 
 watch(anotherData, (newData) => {
   if (newData) {
-    students.value = newData[0]
+    students.value = newData[0].map((student) => student.status == 'Đang học')
     locations.value = newData[1]
     teachers.value = newData[2]
   }
